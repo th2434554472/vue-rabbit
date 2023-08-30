85df8e8e547de095e076b62bfa50782a07609b8e
restia <hello@restia.cn>
2023-08-31 01:17:54 +0800

    详情热榜基本数据渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import DetailHot from './components/DetailHot.vue'
 import {getDetail} from '@/apis/detail'
 import {useRoute} from "vue-router";
 import {onMounted,ref} from "vue";
@@ -116,6 +117,10 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
+              <!--24小时-->
+              <DetailHot/>
+              <!--周-->
+              <DetailHot/>
 
             </div>
           </div>
